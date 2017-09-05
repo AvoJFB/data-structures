1adc0115c725d0ad1684ebd95669dbe5c900fcbd
Avetik Harutyunyan <avetik.harutyunyan1@gmail.com>
2017-09-05 16:43:27 +0300

    Implement Stack data structure

---
+++ Stack/index.js
@@ -0,0 +1,31 @@
+class Stack {
+  constructor(items) {
+    this.items = items || [];
+  }
+
+  push(item) {
+    this.items.push(item);
+    console.log(this.items);
+  }
+
+  pop() {
+    if (this.items.length === 0) {
+      throw new Error('The stack is empty');
+    } else {
+      const res = this.items[this.items.length - 1];
+      this.items = this.items.slice(0, -1);
+      console.log(this.items);
+      return res;
+    }
+  }
+
+  peek() {
+    if (this.items.length === 0) {
+      throw new Error('The stack is empty');
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
+}
+
+module.exports = Stack;
