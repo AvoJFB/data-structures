a63a7324ed55b8042e51538b124fb979d2d6f89d
Avetik Harutyunyan <avetik.harutyunyan1@gmail.com>
2017-09-06 15:40:03 +0300

    Write tests for MaxHeap

---
+++ test/maxHeap.js
@@ -0,0 +1,61 @@
+const { assert } = require('chai');
+const MaxHeap = require('../MaxHeap');
+
+describe('MaxHeap', () => {
+  it('should return empty Heap when no arguments', () => {
+    const maxHeap = new MaxHeap();
+    assert.equal(maxHeap.count(), 0);
+  });
+
+  it('should return length of the Heap on count', () => {
+    const maxHeap = new MaxHeap();
+    maxHeap.push(1);
+    maxHeap.push(2);
+    maxHeap.push(1);
+    assert.equal(maxHeap.count(), 3);
+  });
+
+  it('should push new element to the heap', () => {
+    const maxHeap = new MaxHeap();
+    maxHeap.push(1);
+    assert.deepEqual(maxHeap.toArray(), [1]);
+  });
+
+  it('should sort heap correctly on push', () => {
+    const maxHeap = new MaxHeap();
+    maxHeap.push(1);
+    maxHeap.push(2);
+    maxHeap.push(7);
+    maxHeap.push(4);
+    maxHeap.push(10);
+    maxHeap.push(3);
+    assert.deepEqual(maxHeap.toArray(), [10, 7, 3, 1, 4, 2]);
+  });
+
+  it('should remove top element on pop and sort heap correctly', () => {
+    const maxHeap = new MaxHeap(['a', 'b', 'c']);
+    maxHeap.push(1);
+    maxHeap.push(2);
+    maxHeap.push(7);
+    maxHeap.push(4);
+    maxHeap.push(10);
+    maxHeap.push(3);
+    maxHeap.pop();
+    assert.deepEqual(maxHeap.toArray(), [7, 4, 3, 1, 2]);
+  });
+
+  it('should throw error when calling pop on empty heap', () => {
+    const maxHeap = new MaxHeap();
+    assert.throws(maxHeap.pop, Error);
+  });
+
+  it('should throw error when calling push with NaN argument', () => {
+    const maxHeap = new MaxHeap();
+    assert.throws(() => maxHeap.push('a'), Error);
+  });
+
+  it('should throw error when calling push without argument', () => {
+    const maxHeap = new MaxHeap();
+    assert.throws(maxHeap.push, Error);
+  });
+});
